80b7af33c7387571c4dfd4720bb766b89d19e5bd
trieuquangphuc123 <trieuquangphuc123@gmail.com>
2015-03-27 11:45:06 +0000

    COMPANY

--- module/followedpost/include/component/ajax/ajax.class.php
+++ module/followedpost/include/component/ajax/ajax.class.php
@@ -15,9 +15,27 @@
             if (Phpfox::getService('followedpost')->makeFollowed($iFeedId)){
                 $this->call("$('#followedpost_makeFollowed_$iFeedId').css(\"display\",\"none\");");
                 $this->call("$('#followedpost_makePublic_$iFeedId').css(\"display\", \"block\");");
+
+                $this->_updateFollowCount($iFeedId);
             }
         }
-        
+
+        private function _updateFollowCount($iFeedId)
+        {
+            $iFollow = Phpfox::getService('followedpost')->countFollow($iFeedId);
+            if($iFollow)
+            {
+                $sText = '<p style="color: #63799F">Followed by '.$iFollow.' wayter'.($iFollow > 1 ? 's' : '').'</p>';
+                $this->call("$('#followedpost_makePublic_$iFeedId').closest('ul').find('.follow_number').show().html('".$sText."');");
+                $this->call("$('#followedpost_makePublic_$iFeedId').closest('ul').find('.follow_line').show();");
+            }
+            else
+            {
+                $this->call("$('#followedpost_makePublic_$iFeedId').closest('ul').find('.follow_number').hide();");
+                $this->call("$('#followedpost_makePublic_$iFeedId').closest('ul').find('.follow_line').hide();");
+            }
+        }
+
         public function makePublic()
         {
             $iFeedId = $this->get('id');
@@ -25,6 +43,7 @@
                 $this->call("$('#followedpost_makePublic_$iFeedId').css(\"display\", \"none\");");
                 $this->call("$('#followedpost_makeFollowed_$iFeedId').css(\"display\", \"block\");");
                 $this->call('if(oParams.sController == "followedpost.index"){$("#followedpost_makeFollowed_'. $iFeedId .'").closest(".js_feed_view_more_entry_holder").hide();}');
+                $this->_updateFollowCount($iFeedId);
             }
         }
 
@@ -34,7 +53,7 @@
             {
                 define('PAGE_TIME_LINE', true);
             }
-  
+
             Phpfox::getComponent('followedpost.index', null, 'controller');   
 
             $sYear = $this->get('year');
@@ -46,7 +65,6 @@
             }
             else
             {
-                // $this->html('#js_timeline_year_holder_' . $sYear . '', $this->getContent(false));
                 $this->call('$.scrollTo(\'.timeline_left\', 800);');
                 $this->html('#js_feed_content', $this->getContent(false));
             }
--- module/followedpost/include/plugin/feed.template_block_entry_2.php
+++ module/followedpost/include/plugin/feed.template_block_entry_2.php
@@ -11,50 +11,53 @@
     $aProfileUser ="";
     if(isset($this->_aVars['aUser']))
     {
-     $aProfileUser = $this->_aVars['aUser'];
+        $aProfileUser = $this->_aVars['aUser'];
     }
-     if (isset($sController) && $sController != 'profile.index' || isset($aProfileUser['user_id']) && $aProfileUser['user_id']!= Phpfox::getUserId()||(isset($sView) && $sView == 'followed'))
-     {
-    $bFollowed = Phpfox::getService('followedpost')->isFollowed($this->_aVars['aFeed']['feed_id']);
-    $iFollow = Phpfox::getService('followedpost')->countFollow($this->_aVars['aFeed']['feed_id']);
-    if($bFollowed){
-        $iFollow = (int) $iFollow -0;
-    }
-?>
-<?php if ($iFollow > 0) {?>
-<li>
-        <p style="color: #63799F">Followed by <label><?php echo $iFollow;?></label> wayter<?php if($iFollow > 1) { echo 's'; } ?></p>
-</li>
-<li><span>|</span></li>
-<?php }else{ ?>
-<li style="display: none;">
-        <p style="color: #63799F">Followed by <label></label> wayters</p>
-</li>
-<?php } ?>
-<?php if (!$bFollowed) { ?>
-    <li id='followedpost_makeFollowed_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style=" display: block;">
-        <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makeFollowed', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
-            <?php echo Phpfox::getPhrase('followedpost.followed'); ?>
-        </a>
-    </li>
-    <li id='followedpost_makePublic_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style=" display: none;">
-        <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makePublic', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
-            <?php echo Phpfox::getPhrase('followedpost.public'); ?>
-        </a>
-    </li>
-    <!--<li><span>|</span></li>-->
-    <?php }else{ ?>
-    <li id='followedpost_makePublic_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style="display: block;">
-        <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makePublic', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
-            <?php echo Phpfox::getPhrase('followedpost.public'); ?>
-        </a>
-    </li>
-    <li id='followedpost_makeFollowed_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style="display: none;">
-        <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makeFollowed', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
-            <?php echo Phpfox::getPhrase('followedpost.followed'); ?>
-        </a>
-    </li>
-    <!--<li><span>|</span></li>-->
-<?php }} ?>
+    if (isset($sController) && $sController != 'profile.index' || isset($aProfileUser['user_id']) && $aProfileUser['user_id']!= Phpfox::getUserId()||(isset($sView) && $sView == 'followed'))
+    {
+        $bFollowed = Phpfox::getService('followedpost')->isFollowed($this->_aVars['aFeed']['feed_id']);
+        $iFollow = Phpfox::getService('followedpost')->countFollow($this->_aVars['aFeed']['feed_id']);
+        if($bFollowed){
+            $iFollow = (int) $iFollow -0;
+        }
+    ?>
+    <?php if ($iFollow > 0) {?>
+        <li class="follow_number">
+            <p style="color: #63799F">Followed by <label><?php echo $iFollow;?></label> wayter<?php if($iFollow > 1) { echo 's'; } ?></p>
+        </li>
+        <li class="follow_line"><span>|</span></li>
+        <?php }else{ ?>
+        <li class="follow_number" style="display: none;">
+            <p style="color: #63799F">Followed by <label>0</label> wayters</p>
+        </li>
+        <li class="follow_line" style="display: none;"><span >|</span></li>
+        <?php } ?>
+        
+    <?php if (!$bFollowed) { ?>
+        <li id='followedpost_makeFollowed_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style=" display: block;">
+            <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makeFollowed', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
+                <?php echo Phpfox::getPhrase('followedpost.followed'); ?>
+            </a>
+        </li>
+        <li id='followedpost_makePublic_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style=" display: none;">
+            <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makePublic', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
+                <?php echo Phpfox::getPhrase('followedpost.public'); ?>
+            </a>
+        </li>
+        <!--<li><span>|</span></li>-->
+        <?php }else{ ?>
+        <li id='followedpost_makePublic_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style="display: block;">
+            <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makePublic', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
+                <?php echo Phpfox::getPhrase('followedpost.public'); ?>
+            </a>
+        </li>
+        <li id='followedpost_makeFollowed_<?php echo $this->_aVars['aFeed']['feed_id']; ?>' style="display: none;">
+            <a href="javascript:void(0)" onclick="$.ajaxCall('followedpost.makeFollowed', 'id=<?php echo $this->_aVars['aFeed']['feed_id']; ?>', 'GET')">
+                <?php echo Phpfox::getPhrase('followedpost.followed'); ?>
+            </a>
+        </li>
+        <!--<li><span>|</span></li>-->
+        <?php }
+} ?>
 
     
