be78dd2a1e938a4b88c4001751e22e54b23398c8
trieuquangphuc123 <trieuquangphuc123@gmail.com>
2015-06-12 15:43:52 +0000

    COMPANY

--- module/customprofiles/template/default/block/menu.html.php
+++ module/customprofiles/template/default/block/menu.html.php
@@ -39,22 +39,6 @@
 <ul class="desoft_menu">
     <li {if empty($sView) && $sController == 'profile.index'}class="active"{/if}><a href="{url link=$aUser.user_name}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/wall.png"><span class="js_hover_info">My Post</span></a></li>
     
-    {if Phpfox::isModule('followedpost')}
-    <li {if $sView=='followed'}class="active"{/if}><a href="{url link=$aUser.user_name view='followed'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/follow.png"><span class="js_hover_info">Follow Post</span></a></li>
-    {/if}
-    
-    {if Phpfox::isModule('privatepost')}
-    <li {if $sView=='private'}class="active"{/if}><a href="{url link=$aUser.user_name view='private'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/private.png"><span class="js_hover_info">Private Post</span></a></li>
-    {/if}
-    
-    {if Phpfox::isModule('strongbox')}
-    <li {if $sView=='strongbox'}class="active"{/if}><a href="{url link=$aUser.user_name view='strongbox'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/strongbox.png"><span class="js_hover_info">My Strong Box</span></a></li>
-    {/if}
-    
-    {if Phpfox::isModule('customprofiles')}
-    <li {if $sView=='expire'}class="active"{/if}><a href="{url link=$aUser.user_name view='expire'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/expire.png"><span class="js_hover_info">Expiring posts</span></a></li>
-    {/if}
-    
     {if Phpfox::isModule('anonymousdone')}
     <li {if $sView=='anonydone'}class="active"{/if}><a href="{url link=$aUser.user_name view='anonydone'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/anonymousdone.png"><span class="js_hover_info">Anonymous Post Done</span></a></li>
     {/if}
@@ -63,12 +47,29 @@
     <li {if $sView=='anonyreceived'}class="active"{/if}><a href="{url link=$aUser.user_name view='anonyreceived'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/anonymousreceive.png"><span class="js_hover_info">Anonymous Post Receive</span></a></li>
     {/if}
     
-    {if Phpfox::isModule('waytime') && false}
-    <li {if $sController == 'waytime.profile'}class="active"{/if}><a href="{url link=$aUser.user_name.'.waytime'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/waytime.png"><span class="js_hover_info">W-Time Capsule</span></a></li>
+    {if Phpfox::isModule('privatepost')}
+    <li {if $sView=='private'}class="active"{/if}><a href="{url link=$aUser.user_name view='private'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/private.png"><span class="js_hover_info">Private Post</span></a></li>
+    {/if}
+        
+    {if Phpfox::isModule('strongbox')}
+    <li {if $sView=='strongbox'}class="active"{/if}><a href="{url link=$aUser.user_name view='strongbox'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/strongbox.png"><span class="js_hover_info">My Strong Box</span></a></li>
+    {/if}
+    
+    {if Phpfox::isModule('followedpost')}
+    <li {if $sView=='followed'}class="active"{/if}><a href="{url link=$aUser.user_name view='followed'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/follow.png"><span class="js_hover_info">Follow Post</span></a></li>
     {/if}
     
     {if Phpfox::isModule('waytame')}
     <li {if $sController=='waytame.profile'}class="active"{/if}><a href="{url link=$aUser.user_name.'.waytame'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/waytame.png"><span class="js_hover_info">Waytame</span></a></li>
     {/if}
+    
+    {if Phpfox::isModule('customprofiles')}
+    <li {if $sView=='expire'}class="active"{/if}><a href="{url link=$aUser.user_name view='expire'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/expire.png"><span class="js_hover_info">Expiring posts</span></a></li>
+    {/if}
+    
+    {if Phpfox::isModule('waytime') && false}
+    <li {if $sController == 'waytime.profile'}class="active"{/if}><a href="{url link=$aUser.user_name.'.waytime'}" class="js_hover_title"><img src="{param var='core.path'}module/customprofiles/static/image/icons/waytime.png"><span class="js_hover_info">W-Time Capsule</span></a></li>
+    {/if}
+    
     <li class="clear"></li>
 </ul>
